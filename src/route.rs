001508977e821d96b1ca72f944784c1b2f4673f2
Eliza Weisman <eliza@buoyant.io>
2022-09-12 15:44:42 -0700

    index HTTPRoutes and bind them to Servers (#3)
    This branch adds support for indexing HTTPRoute resources to the
    prototype client-policy controller. Next, we'll want to also index the
    new CRDs and bind them to HTTPRoutes as well, but that will happen in my
    next PR.
    
    I also fixed a bug where the controller didn't populate pods with
    default servers when they were created. I should have done this in a
    separate branch, but 🤷‍♀️
    
    ![image](https://user-images.githubusercontent.com/2796466/189503986-443a3a90-33fc-4e25-8274-0f52ac23610b.png)

--- src/route.rs
+++ src/route.rs
@@ -1,5 +1,11 @@
 pub use crate::core::http_route::*;
+use crate::k8s::{
+    self,
+    policy::{httproute as policy, Server},
+};
+use anyhow::{anyhow, Error, Result};
 use chrono::{offset::Utc, DateTime};
+use k8s_gateway_api as api;
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct OutboundHttpRoute {
@@ -12,12 +18,244 @@ pub struct OutboundHttpRoute {
     pub creation_timestamp: Option<DateTime<Utc>>,
 }
 
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct OutboundRouteBinding {
+    pub parents: Vec<OutboundParentRef>,
+    pub route: OutboundHttpRoute,
+}
+
 impl Default for OutboundHttpRoute {
     fn default() -> Self {
         Self {
             hostnames: Vec::new(),
             rules: Vec::new(),
             creation_timestamp: None,
+        }
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum OutboundParentRef {
+    Server(String),
+}
+
+#[derive(Clone, Debug, thiserror::Error)]
+pub enum InvalidParentRef {
+    #[error("HTTPRoute resource does not reference a Server resource")]
+    DoesNotSelectServer,
+
+    #[error("HTTPRoute resource may not reference a parent Server in an other namespace")]
+    ServerInAnotherNamespace,
+
+    #[error("HTTPRoute resource may not reference a parent by port")]
+    SpecifiesPort,
+
+    #[error("HTTPRoute resource may not reference a parent by section name")]
+    SpecifiesSection,
+}
+
+impl TryFrom<policy::HttpRoute> for OutboundRouteBinding {
+    type Error = Error;
+
+    fn try_from(route: policy::HttpRoute) -> Result<Self, Self::Error> {
+        let route_ns = route.metadata.namespace.as_deref();
+        let creation_timestamp = route.metadata.creation_timestamp.map(|k8s::Time(t)| t);
+        let parents = OutboundParentRef::collect_from(route_ns, route.spec.inner.parent_refs)?;
+        let hostnames = route
+            .spec
+            .hostnames
+            .into_iter()
+            .flatten()
+            .map(convert::host_match)
+            .collect();
+
+        let rules = route
+            .spec
+            .rules
+            .into_iter()
+            .flatten()
+            .map(|policy::HttpRouteRule { matches, filters }| Self::try_rule(matches, filters))
+            .collect::<Result<_>>()?;
+
+        Ok(OutboundRouteBinding {
+            parents,
+            route: OutboundHttpRoute {
+                hostnames,
+                rules,
+                creation_timestamp,
+            },
+        })
+    }
+}
+
+impl OutboundRouteBinding {
+    #[inline]
+    pub fn selects_server(&self, name: &str) -> bool {
+        self.parents
+            .iter()
+            .any(|p| matches!(p, OutboundParentRef::Server(n) if n == name))
+    }
+
+    fn try_rule<F>(
+        matches: Option<Vec<api::HttpRouteMatch>>,
+        filters: Option<Vec<F>>,
+    ) -> Result<InboundHttpRouteRule> {
+        let matches = matches
+            .into_iter()
+            .flatten()
+            .map(Self::try_match)
+            .collect::<Result<_>>()?;
+
+        // TODO(eliza): how will we figure out what route rules are applied on
+        // the outbound side? Are we even gonna do that?
+        // let filters = filters
+        //     .into_iter()
+        //     .flatten()
+        //     .map(try_filter)
+        //     .collect::<Result<_>>()?;
+
+        Ok(InboundHttpRouteRule {
+            matches,
+            filters: Vec::new(),
+        })
+    }
+
+    fn try_match(
+        api::HttpRouteMatch {
+            path,
+            headers,
+            query_params,
+            method,
+        }: api::HttpRouteMatch,
+    ) -> Result<HttpRouteMatch> {
+        let path = path.map(convert::path_match).transpose()?;
+
+        let headers = headers
+            .into_iter()
+            .flatten()
+            .map(convert::header_match)
+            .collect::<Result<_>>()?;
+
+        let query_params = query_params
+            .into_iter()
+            .flatten()
+            .map(convert::query_param_match)
+            .collect::<Result<_>>()?;
+
+        let method = method.as_deref().map(Method::try_from).transpose()?;
+
+        Ok(HttpRouteMatch {
+            path,
+            headers,
+            query_params,
+            method,
+        })
+    }
+}
+
+impl OutboundParentRef {
+    fn collect_from(
+        route_ns: Option<&str>,
+        parent_refs: Option<Vec<api::ParentReference>>,
+    ) -> Result<Vec<Self>, InvalidParentRef> {
+        let parents = parent_refs
+            .into_iter()
+            .flatten()
+            .filter_map(|parent_ref| Self::from_parent_ref(route_ns, parent_ref))
+            .collect::<Result<Vec<_>, InvalidParentRef>>()?;
+
+        // If there are no valid parents, then the route is invalid.
+        if parents.is_empty() {
+            return Err(InvalidParentRef::DoesNotSelectServer);
+        }
+
+        Ok(parents)
+    }
+
+    fn from_parent_ref(
+        route_ns: Option<&str>,
+        parent_ref: api::ParentReference,
+    ) -> Option<Result<Self, InvalidParentRef>> {
+        // Skip parent refs that don't target a `Server` resource.
+        if !policy::parent_ref_targets_kind::<Server>(&parent_ref) || parent_ref.name.is_empty() {
+            return None;
+        }
+
+        let api::ParentReference {
+            group: _,
+            kind: _,
+            namespace,
+            name,
+            section_name,
+            port,
+        } = parent_ref;
+
+        if namespace.is_some() && namespace.as_deref() != route_ns {
+            return Some(Err(InvalidParentRef::ServerInAnotherNamespace));
+        }
+        if port.is_some() {
+            return Some(Err(InvalidParentRef::SpecifiesPort));
+        }
+        if section_name.is_some() {
+            return Some(Err(InvalidParentRef::SpecifiesSection));
+        }
+
+        Some(Ok(OutboundParentRef::Server(name)))
+    }
+}
+
+pub mod convert {
+    use super::*;
+
+    pub fn path_match(path_match: api::HttpPathMatch) -> Result<PathMatch> {
+        match path_match {
+            api::HttpPathMatch::Exact { value } | api::HttpPathMatch::PathPrefix { value }
+                if !value.starts_with('/') =>
+            {
+                Err(anyhow!("HttpPathMatch paths must be absolute (begin with `/`); {value:?} is not an absolute path"))
+            }
+            api::HttpPathMatch::Exact { value } => Ok(PathMatch::Exact(value)),
+            api::HttpPathMatch::PathPrefix { value } => Ok(PathMatch::Prefix(value)),
+            api::HttpPathMatch::RegularExpression { value } => value
+                .parse()
+                .map(PathMatch::Regex)
+                .map_err(Into::into),
+        }
+    }
+
+    pub fn host_match(hostname: api::Hostname) -> HostMatch {
+        if hostname.starts_with("*.") {
+            let mut reverse_labels = hostname
+                .split('.')
+                .skip(1)
+                .map(|label| label.to_string())
+                .collect::<Vec<String>>();
+            reverse_labels.reverse();
+            HostMatch::Suffix { reverse_labels }
+        } else {
+            HostMatch::Exact(hostname)
+        }
+    }
+
+    pub fn header_match(header_match: api::HttpHeaderMatch) -> Result<HeaderMatch> {
+        match header_match {
+            api::HttpHeaderMatch::Exact { name, value } => {
+                Ok(HeaderMatch::Exact(name.parse()?, value.parse()?))
+            }
+            api::HttpHeaderMatch::RegularExpression { name, value } => {
+                Ok(HeaderMatch::Regex(name.parse()?, value.parse()?))
+            }
+        }
+    }
+
+    pub fn query_param_match(query_match: api::HttpQueryParamMatch) -> Result<QueryParamMatch> {
+        match query_match {
+            api::HttpQueryParamMatch::Exact { name, value } => {
+                Ok(QueryParamMatch::Exact(name, value))
+            }
+            api::HttpQueryParamMatch::RegularExpression { name, value } => {
+                Ok(QueryParamMatch::Exact(name, value.parse()?))
+            }
         }
     }
 }
